82aeec7204542590f1776600d41af58c29732a12
David Russell <russeldj@clarkson.edu>
2020-11-09 08:11:57 -0500

    Added constants for new data

--- constants.py
+++ constants.py
@@ -14,6 +14,13 @@ SMOKE_FOLDERS = ["first_computer_full3D",
 SMOKE_FOLDERS = [join(data_dir, smoke_folder)
                  for smoke_folder in SMOKE_FOLDERS]
 
+SMOKE_FOLDERS_LONG = ["laptop_1000_steps", "wall_computer_1000_steps"]
+
+# prepend the absolute path of the data directory
+SMOKE_FOLDERS_LONG = [join(data_dir, smoke_folder)
+                      for smoke_folder in SMOKE_FOLDERS_LONG]
+
+
 # The folder names for each of the false alarm folders
 FALSE_ALARM_FOLDERS = ["bike_full3D"]
 
@@ -21,6 +28,11 @@ FALSE_ALARM_FOLDERS = ["bike_full3D"]
 FALSE_ALARM_FOLDERS = [join(data_dir, false_alarm_folder)
                        for false_alarm_folder in FALSE_ALARM_FOLDERS]
 
+FALSE_ALARM_FOLDERS_LONG = ["bike_1000_steps"]
+
+# prepend the absolute path of the data directory
+FALSE_ALARM_FOLDERS_LONG = [join(data_dir, false_alarm_folder)
+                            for false_alarm_folder in FALSE_ALARM_FOLDERS_LONG]
+
 NASA_DETECTORS = (2.11500001, -3., -2.84649992, 6.42500019, 0., -2.84649992)
-#SOURCE_LOCATIONS=[1.9866, -0.5119, -1.1846, 2.2185, -2.5768, -1.0527, 7.279, -.0300, -1.1963]
-SOURCE_LOCATIONS=[1.9866, -0.5119, -1.1846, 2.2185, -2.5768, -1.0527, 7.279, -3.0, -1.1963]
+SOURCE_LOCATIONS=(1.9866, -0.5119, -1.1846, 2.2185, -2.5768, -1.0527, 7.279, -3.0, -1.1963)
